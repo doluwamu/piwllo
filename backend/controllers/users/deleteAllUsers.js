279eb401bb26d9c0e7ac4a4dd73a3e7cc017b658
doluwamu <dboyadeitan@gmail.com>
2022-03-12 20:44:24 +0100

    made change in deleteAllUsers endpoint

--- backend/controllers/users/deleteAllUsers.js
+++ backend/controllers/users/deleteAllUsers.js
@@ -1,12 +1,17 @@
-import User from "../../models/userModel";
+import AppError from "../../error/appError.js";
+import User from "../../models/userModel.js";
 
 // Request type: DELETE
 // To: /api/v1/users/delete
 // Desc: to delete all users from db(Admins Only)
 const deleteAllUsers = async (req, res, next) => {
   try {
-    await User.deleteMany({});
-    return res.json({ message: "All users successfully deleted" });
+    const deleted = await User.deleteMany({});
+    if (deleted) {
+      return res.json({ message: "All users successfully deleted" });
+    } else {
+      return next(new AppError("Unable to delete users!", 404));
+    }
   } catch (error) {
     return next(error);
   }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import deleteAllUsers from "../controllers/users/deleteAllUsers.js";
 import deleteUser from "../controllers/users/deleteUser.js";
 import getUsers from "../controllers/users/getUsers.js";
 import {
@@ -12,6 +13,7 @@ const router = express.Router();
 router.get("/user/profile", protect, getUserProfile);
 router.put("/user/profile/edit", protect, updateProfile);
 router.delete("/user/delete", protect, deleteUser);
+router.delete("", protect, isAdmin, deleteAllUsers);
 router.get("", protect, isAdmin, getUsers);
 
 export default router;
