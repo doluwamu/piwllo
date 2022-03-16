081b8a10d2df4ca49e34627a7d4e8e9931fc594b
doluwamu <dboyadeitan@gmail.com>
2022-03-16 16:05:37 +0100

    created template for review feature

--- frontend/src/screens/AddRewiewScreen.js
+++ frontend/src/screens/AddRewiewScreen.js
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import AsideBar from "../components/AsideBar";
+import ThemeToggleButton from "../components/ThemeToggleButton";
+import { ThemeContext } from "../context/ThemeContext";
 
 const AddRewiewScreen = () => {
+  const { darkTheme } = useContext(ThemeContext);
+  const [review, setReview] = useState("");
+
+  const handleReview = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="add-review-section main">
       {/* Aside bar */}
       <AsideBar />
 
-      <div></div>
+      <div
+        className={`review-section general-section ${
+          darkTheme ? "dark" : "light"
+        }`}
+      >
+        {/* Theme tuggle button */}
+        <div className="theme-btn-section">
+          <div className="theme-btn-container">
+            <ThemeToggleButton />
+          </div>
+        </div>
+
+        {/* Review */}
+        <div className="review">
+          <textarea
+            placeholder="Drop a review message"
+            value={review}
+            onChange={(e) => setReview(e.target.value)}
+          ></textarea>
+          <div className="review-btn-section">
+            <button className="btn-review" type="submit" onClick={handleReview}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
