763dd5ff302b4b4c88e48efacdbbe294d66ccaaa
doluwamu <dboyadeitan@gmail.com>
2022-03-19 15:35:08 +0100

    created pages for viewing task priorities

--- frontend/src/AllRoutes.js
+++ frontend/src/AllRoutes.js
@@ -4,6 +4,9 @@ import HomeScreen from "./screens/HomeScreen";
 import SignupScreen from "./screens/SignupScreen";
 import SigninScreen from "./screens/SigninScreen";
 import TaskManagerScreen from "./screens/TaskManagerScreen";
+import ImportantTasksScreen from "./screens/ImportantTasksScreen";
+import VeryImportantTasksScreen from "./screens/VeryImportantTasksScreen";
+import PriorityTasksScreen from "./screens/PriorityTasksScreen";
 import ProfileViewScreen from "./screens/ProfileViewScreen";
 import ProfileEditScreen from "./screens/ProfileEditScreen";
 import AddRewiewScreen from "./screens/AddRewiewScreen";
@@ -19,6 +22,9 @@ const AllRoutes = () => {
         <Route path="/user/profile" element={<ProfileViewScreen />} />
         <Route path="/user/profile/edit" element={<ProfileEditScreen />} />
         <Route path="/task-manager" element={<TaskManagerScreen />} />
+        <Route path="/tasks/important" element={<ImportantTasksScreen />} />
+        <Route path="/tasks/very-important" element={<VeryImportantTasksScreen />} />
+        <Route path="/tasks/priority" element={<PriorityTasksScreen />} />
         <Route path="/teams/create" element={<CreateTeamScreen />} />
         <Route path="/review" element={<AddRewiewScreen />} />
       </Routes>
---
+++ frontend/src/screens/PriorityTasksScreen.js
@@ -0,0 +1,75 @@
+import React, {  useContext } from "react";
+// import { Link } from "react-router-dom";
+import AsideBar from "../components/AsideBar";
+import ThemeToggleButton from "../components/ThemeToggleButton";
+import { ThemeContext } from "../context/ThemeContext";
+
+const PriorityTasksScreen = () => {
+
+  const { darkTheme } = useContext(ThemeContext);
+
+  return (
+    <div className="task-manager-section main">
+      {/* Aside bar */}
+      <AsideBar />
+
+      {/* task actions */}
+      <div
+        className={`task-section general-section ${
+          darkTheme ? "dark" : "light"
+        }`}
+      >
+        {/* Theme tuggle button */}
+        <div className="theme-btn-section">
+          <div className="theme-btn-container">
+            <ThemeToggleButton />
+          </div>
+        </div>
+
+        <h1>Priority Tasks</h1>
+
+        <div className="task-actions-section">
+          <div className="show-tasks-section">
+            <table>
+              <thead>
+                <tr>
+                  <th>Task</th>
+                  <th className="priority">Priority level</th>
+                  <th className="btns"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Buy Suya</td>
+                  <td className="priority">Priority</td>
+                  <td>
+                    <button type="button" className="btn-edit">
+                      <i className="fas fa-edit"></i>
+                    </button>
+                    <button type="button" className="btn-delete">
+                      <i className="fas fa-trash"></i>{" "}
+                    </button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Buy Suya</td>
+                  <td className="priority">Priority</td>
+                  <td>
+                    <button type="button" className="btn-edit">
+                      <i className="fas fa-edit"></i>
+                    </button>
+                    <button type="button" className="btn-delete">
+                      <i className="fas fa-trash"></i>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PriorityTasksScreen;
