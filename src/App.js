ff35d616145765b5ef67526a905d1c17f7a4adac
doluwamu <dboyadeitan@gmail.com>
2022-04-27 06:54:46 +0100

    made fixes before deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useContext } from "react";
 import AllRoutes from "./AllRoutes";
 import { ThemeContext } from "./context/ThemeContext";
+import Meta from "./components/Meta";
 
 const App = () => {
   const { darkTheme } = useContext(ThemeContext);
 
   return (
     <div className={`canvas ${darkTheme ? "canvas-dark" : "canvas-light"}`}>
+      <Meta />
       <AllRoutes />
     </div>
   );
