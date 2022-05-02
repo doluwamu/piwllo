9d3d9bcb655b431ddc7ff0412359dd85e9a2cb76
doluwamu <dboyadeitan@gmail.com>
2022-05-02 17:29:47 +0100

    improving login feature

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -32,8 +32,8 @@ const SigninScreen = () => {
 
   useEffect(() => {
     if (userDetails) {
-      window.location.reload();
-      return navigate(redirect);
+      return window.location.reload();
+      // return navigate(redirect);
     }
 
     if (userEmail) setEmail(userEmail);
